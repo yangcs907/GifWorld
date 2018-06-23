0839a5af5210f2fa1d2c38ff1e4515d875c0d14c
Chris Yang <chrisyang@Chriss-MacBook-Pro.local>
2018-06-23 03:51:13 -0400

    changes

--- client/src/Auth.js
+++ client/src/Auth.js
@@ -6,7 +6,7 @@ export default class Auth {
     this.auth0 = new auth0.WebAuth({
       domain: 'yangcs907.auth0.com',
       clientID: 'vkPaC4Qh3JhQCl2s1lnkL5c36VHToUaP',
-      redirectUri: "https://gif-world.herokuapp.com/dashboard",
+      redirectUri: "https://gif-world.herokuapp.com/callback",
       //'http://localhost:3000/callback',
       // redirectUri: 'https://warm-reef-54993.herokuapp.com/callback',
       audience: 'https://yangcs907.auth0.com/api/v2/',
