53110d6550e39a7ef51cc669194bac898a3a4850
Chris Yang <chrisyang@Chriss-MacBook-Pro.local>
2018-07-04 16:27:53 -0400

    changes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const env = process.env.NODE_ENV || 'dev';
 
 // Let's get the cloud url and save that to a variable
 const mlab_url = 'mongodb://heroku_d9hdg32c:qurm274p03n438kkuibuhmau4l@ds011311.mlab.com:11311/heroku_d9hdg32c';
-//`mongodb://${config.db_username}:${config.db_pass}@ds117605.mlab.com:17605/test_db`;
+//const db_urll = `mongodb://${config.db_username}:${config.db_pass}@ds117605.mlab.com:17605/test_db`;
 const db_url = 'mongodb://localhost/GifWorld';
 
 // Now we can switch the urls based on if we're in production(on Heroku, etc)
